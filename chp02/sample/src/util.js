1f60fa2103174bb95c34c75145246e5350581601
opblang <opblang@mail.com>
2016-12-07 12:30:51 +0700

    Working on chp02

---
+++ chp02/sample/src/util.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  this.f2c =  () => {
+    return 0
+  }
+}
+
+
---
+++ chp02/sample/test/util_test.js
@@ -0,0 +1,29 @@
+let expect = require('chai').expect
+let Util = require('../src/util')
+
+describe('util test', () => {
+  it('should pass this canary test', () => {
+    expect(true).to.eql(true)
+  })
+})
+
+let util
+
+beforeEach(() => {
+  util = new Util()
+})
+
+it('should pass if f2c return 0C for 32F', () => {
+  let fahrenheit = 32
+  let celcius = util.f2c(fahrenheit)
+
+  expect(celcius).to.eql(0)
+})
+
+
+it('should pass if f2c return 10C for 50F', () => {
+  let fahrenheit = 32
+  let celcius = util.f2c(fahrenheit)
+
+  expect(celcius).to.eql(0)
+})
